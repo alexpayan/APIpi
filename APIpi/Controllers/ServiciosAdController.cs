96a5b9aa4a364910803a82a6da6d08ce97c9951e
Nathalia Rivera <nathalia.rivera@utp.ac.pa>
2024-11-25 15:04:41 -0500

    ServiciosAdController
    Una parte del controller para este recurso :bee:

---
+++ APIpi/Controllers/ServiciosAdController.cs
@@ -0,0 +1,42 @@
+﻿using APIpi.Controllers.LocacionTypes;
+using APIpi.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIpi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServiciosAdController : ControllerBase
+    {
+        private readonly ILogger<LocacionController> _logger;
+        private readonly AppDbContext _context;
+
+        /*[HttpPost(Name = "PostServiciosA")]
+        public async Task<ActionResult<PostLocacionResponse>> Post(PostLocacionRequest request)
+        {
+            var locacion = new Locacion
+            {
+                Nombre_Locacion = request.Nombre_Locacion,
+                Tipo_Locacion = request.Tipo_Locacion,
+                Capacidad_Maxima = request.Capacidad_Maxima,
+                Dirección = request.Dirección,
+                Precio_Base = request.Precio_Base
+            };
+            _context.Locaciones.Add(locacion);
+            await _context.SaveChangesAsync();
+
+            var response = new PostLocacionResponse
+            {
+                ID_Locacion = locacion.ID_Locacion,
+                Nombre_Locacion = locacion.Nombre_Locacion,
+                Tipo_Locacion = locacion.Tipo_Locacion,
+                Capacidad_Maxima = locacion.Capacidad_Maxima,
+                Dirección = locacion.Dirección,
+                Precio_Base = locacion.Precio_Base
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = locacion.ID_Locacion }, locacion);
+        }*/
+    }
+}
