f8b320f52c9af9c937aaa190abbcf12b0bd5d6da
Ivan Nuñez <ivanalbertonunez@gmail.com>
2024-11-25 17:50:49 -0500

    se agregan todos los archivos relacionados con Usuario

---
+++ APIpi/Controllers/UsuarioController/PostUserRequest.cs
@@ -0,0 +1,35 @@
+﻿using APIpi.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace APIpi.Controllers.UsuarioController
+{
+    public class PostUserRequest
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Apellido { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Correo_Electrónico { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Contraseña { get; set; }
+
+        [MaxLength(255)]
+        public string Teléfono { get; set; }
+
+        [MaxLength(255)]
+        public string Dirección { get; set; }
+
+        [Required]
+        public string Tipo { get; set; }
+    }
+}
--- APIpi/Model/AppDbContext.cs
+++ APIpi/Model/AppDbContext.cs
@@ -8,6 +8,7 @@ namespace APIpi.Model
 
         public DbSet<Locacion> Locaciones { get; set; }
         public DbSet<ServiciosAdicionales> Servicios_Adicionales { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
 
         // Agreagar clase que representa cada tabla en el folder de Model
         // Agregar public DbSet<NombreDeLaClase> NombreDeLaTabla { get; set; } aqui por cada tabla
