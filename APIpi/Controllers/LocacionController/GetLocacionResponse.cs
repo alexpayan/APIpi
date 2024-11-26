bf4f9cbe61caaca0fe3fae7b8464349ac91cd258
Ivan Nuñez <ivanalbertonunez@gmail.com>
2024-11-25 22:08:35 -0500

    se corrige un nombre incorrecto en una clase que no era

--- APIpi/Controllers/LocacionController/GetLocacionResponse.cs
+++ APIpi/Controllers/LocacionController/GetLocacionResponse.cs
@@ -6,7 +6,7 @@ using System.Text.Json.Serialization;
 
 namespace APIpi.Controllers.LocacionTypes
 {
-    public class GetLocacionResponse
+    public class GetEventosResponse
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
